85d8780279d6f553762eb6edf9932f9ae3d217de
AlejoGomezQ <83100536+AlejoGomezQ@users.noreply.github.com>
2023-11-15 13:36:59 -0500

    bugs fixed

--- Assets/Nokrom/Scripts/Scripts/Oleada/Oleadas.cs
+++ Assets/Nokrom/Scripts/Scripts/Oleada/Oleadas.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MoreMountains.Tools;
 
 public class Oleadas : MonoBehaviour
 {
@@ -11,37 +12,45 @@ public class Oleadas : MonoBehaviour
     private GameObject contenedor_grupos;
     public static Oleadas Instance { get; private set; }
     public GameObject ContenedorGrupos { get => contenedor_grupos; set => contenedor_grupos = value; }
+    public int OlaActual { get => ola_actual; set => ola_actual = value; }
 
-
-    private void Awake() 
-    { 
+    public int CantidadOlas()
+    {
+        return olas.Count;
+    }
+    private void Awake()
+    {
         // If there is an instance, and it's not me, delete myself.
-        
-        if (Instance != null && Instance != this) 
-        { 
-            Destroy(this); 
-        } 
-        else 
-        { 
-            Instance = this; 
-        } 
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
     }
 
-    public void Start(){
+    public void Start()
+    {
         DespacharOla();
     }
 
-    public void DespacharOla(){
+    public void DespacharOla()
+    {
         Ola ola;
-        if(ola_actual < olas.Count)
+        if (ola_actual < olas.Count)
         {
             ola = olas[ola_actual];
             ola.EmpezarOla();
             ola.OleadasNivel = this;
             ola_actual++; //Acá se acualiza el contador de olas
-            Debug.Log("Ola "+ola_actual+"/"+olas.Count);
+            //Debug.Log("Ola "+ola_actual+"/"+olas.Count);
+            MMGameEvent.Trigger("ActualizarUIOla");
         }
-        else{
+        else
+        {
             //Se termina la partida indicar que gano y mostrar ventana fin de nivel
             Debug.Log("Fin de oleadas");
         }
